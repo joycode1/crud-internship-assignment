2e6086033fc0e8c9c56caa2b6975d3d93178d670
Radost Chipeva <radost.chipeva@gmail.com>
2020-02-13 11:29:21 +0200

    Added reducer and actions

---
+++ src/store/actions/actions.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import {SUBMIT_START, SUBMIT_SUCCESS, SUBMIT_FAIL} from "./actionTypes";
+
+
+const startSubmit = () => {
+    return {
+        type: SUBMIT_START
+    }
+};
+const successSubmit = (id, applicationData) => {
+    return {
+        type: SUBMIT_SUCCESS,
+        appId: id,
+        applicationData
+    }
+};
+const failSubmit = (error) => {
+    return {
+        type: SUBMIT_FAIL,
+        error
+    }
+};
+
+const submitApplication = (applicationData) => {
+    return dispatch => {
+        dispatch(startSubmit());
+        axios.post('https://student-management-task.firebaseio.com/apllications.json')
+            .then((res) => {
+                dispatch(successSubmit(res.data, applicationData))
+            })
+            .catch(err => dispatch(failSubmit(err)))
+    }
+};
+
+export {
+    submitApplication
+}
--- src/store/reducer/reducer.js
+++ src/store/reducer/reducer.js
@@ -1,10 +1,39 @@
+import {SUBMIT_START, SUBMIT_SUCCESS, SUBMIT_FAIL} from "../actions/actionTypes";
 
-const initialState = {};
+const initialState = {
+    applications: [],
+    loading: false,
+    error: null
+};
 
-const reducer = (state=initialState,actions)=>{
+const reducer = (state = initialState, action) => {
+    const reducerMap = {
+        [SUBMIT_START]: () => {
+            return {
+                ...state,
+                loading: true,
+                error: null
+            }
+        },
+        [SUBMIT_SUCCESS]: () => {
+            const newApplication = {...action.applicationData, appId: action.appId,};
+            return {
+                ...state,
+                applications: [...state.applications, newApplication],
+                loading: false
+            }
+        },
+        [SUBMIT_FAIL]: () => {
+            return {
+                error: action.error
+            }
+        }
+    };
 
+    if (Object.keys(reducerMap).includes(action.type)) {
+        return reducerMap[action.type]();
+    }
     return state;
-
 };
 
 
